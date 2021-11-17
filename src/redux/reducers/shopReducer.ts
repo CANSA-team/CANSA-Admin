fffbe8a2677c77a79156782ba8c4ffec8f983ef4
VanLoc <locpv.46.student@fit.tdc.edu.vn>
2021-11-17 11:16:37 +0700

    Viet redux cho ShopAction

---
+++ src/redux/reducers/shopReducer.ts
@@ -0,0 +1,36 @@
+import { ShopActionType } from "../action-types";
+import { ShopActions } from "../actions/shopActions";
+import { ShopState, ShopModel } from "../models";
+
+
+const initialState: ShopState = {
+    info: {} as ShopModel,
+    shopList: [] as ShopModel[],
+    error: undefined
+}
+
+const shopReducer = (state: ShopState = initialState, action: ShopActions) => {
+    switch (action.type) {
+        case ShopActionType.GET_SHOP_INFO:
+            return {
+                ...state,
+                info: action.payload
+            }
+        case ShopActionType.ON_SHOP_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        case ShopActionType.GET_SHOP_LIST:
+        case ShopActionType.UPDATE_STATUS_SHOP:
+            return {
+                ...state,
+                shopList: action.payload
+            }
+        default:
+            return state;
+
+    }
+}
+
+export default shopReducer;
